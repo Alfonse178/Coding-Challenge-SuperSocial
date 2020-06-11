f7e7a39d6372fb9682f74d2e8b5235d482ee9c91
Alfie Sargent <35934230+Alfonse178@users.noreply.github.com>
2020-06-11 15:40:55 +0100

    JavaSkeletonCode
    An example of skeleton code that can be used if needed. APPLICANTS DONT HAVE TO USE THIS IF THEY DONT WANT TO!

---
+++ SkeletonCodeSSChallenge.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int findDuplicate(int[] nums) {
+
+    int tort = nums[0];
+    int h = nums[0];
+    do {
+
+    } while (tort != h);
+
+    // Find the "entrance" to the cycle.
+    tort = nums[0];
+    while (tort != h) {
+
+    }
+
+    return hare;
+  }
+}
